a74bf0001af75889702e353df2a5bd366b1d00de
SzangDew <lenonard.do@outlook.com>
2019-10-29 19:00:18 +0700

    Update keyboard shortcut and setting files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SimpleTranslate
+This plugin use free API url:
+
+`https://translate.googleapis.com/translate_a/single?client=gtx&sl={SOURCE_LANG}&tl={TARGET_LANG}&dt=t&q={TEXT_NEED_TRANSLATE}`
+
+And just a **test version** and just use on *Window Os*.
+
+# Install
+```cd sublime3\Data\Packages\User
+git clone https://github.com/SzangLenonard/SimpleTranslate
+```
+Then restart your ST3paragraph .
+# Usage
+- Go to **Preferences> Package Settings> Translate> Settings - User** to set the source and target languages. 
+- Select paragraph that you want to translate, press *Alt + F1* and the translated text will appear next to the selected text.
--- translate.py
+++ translate.py
@@ -8,7 +8,6 @@ API_URL = "https://translate.googleapis.com/translate_a/single?client=gtx"
 class TranslateCommand(sublime_plugin.TextCommand):
 	
 	def translate(self,TEXT,SOURCE_LANG,TARGET_LANG):
-		''' '''
 		TEXT = urllib.parse.quote(TEXT.encode("utf-8"))
 		GET_LINK  = "{0}&sl={1}&tl={2}&dt=t&q={3}".format(API_URL,SOURCE_LANG,TARGET_LANG,TEXT)
 		x = urllib.request.urlopen(GET_LINK)
@@ -24,13 +23,14 @@ class TranslateCommand(sublime_plugin.TextCommand):
 			self.settings = sublime.load_settings("Translate.sublime-settings")
 	
 	def run(self, edit):
-		self.loadSetting()
+		self.loadSetting() # Load setting file 
 		view = self.view
 		sel = view.sel()
 		selection = sel[0]
-		selectionText = view.substr(selection)
+		selectionText = view.substr(selection) # Get selection 
 		if len(selectionText) >0:
-			slang = self.settings.get("source_lang")
-			tlang = self.settings.get("target_lang")
+			slang = self.settings.get("source_lang") # source lang config 
+			tlang = self.settings.get("target_lang") # target lang 
 			inserting = self.translate(selectionText,slang,tlang)
-			self.view.insert(edit, selection.end() , " {0}".format(inserting))
+			'''Data after call api'''
+			self.view.insert(edit, selection.end() , " {0}".format(inserting)) # insert to current View window
